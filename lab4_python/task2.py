70cf263db7e4e9dd5f91116384a28c004ac9908d
Lina <116522273+Linasthsmb@users.noreply.github.com>
2023-11-15 21:39:16 +0300

    Update task2.py

--- lab4_python/task2.py
+++ lab4_python/task2.py
@@ -1,18 +1,20 @@
+import csv
 import json
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
 
 
-FILENAME = "input.json"
-
-
-def task() -> list[dict]:
-    with open(FILENAME) as f:
-        json_data = json.load(f)
-    lst = sorted(json_data, key=lambda i: i["id"])
-    return lst
-    ...  # TODO отсортировать и вернуть список словарей
+def task() -> None:
+    with open(INPUT_FILENAME, 'r') as f:
+        reader = csv.DictReader(f, delimiter=',', quotechar='\n')
+        data = [row for row in reader]
+        with open(OUTPUT_FILENAME, 'w') as o:
+            json.dump(data, o, indent=4)
 
 
 if __name__ == '__main__':
-    # Необходимо для проверки
-    data = task()
-    print(json.dumps(data, indent=4))
+    # Нужно для проверки
+    task()
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
